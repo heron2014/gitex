6e937623703399bf213a195f276e8383887497c9
Anita <a.nita@hotmail.co.uk>
2017-02-11 12:07:45 +0000

    feat: sends message from background.js to popup.js and displays on popup.html

---
+++ background.js
@@ -0,0 +1,1 @@
+var status = 'Hello from Gitify!!'
--- popup.js
+++ popup.js
@@ -1,7 +1,13 @@
+function messageFromBackground() {
+  chrome.runtime.getBackgroundPage(function(bg) {
+      renderStatus(bg.status);
+  });
+}
+
 function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  renderStatus('Hello from Gitify');
+  messageFromBackground();
 });
